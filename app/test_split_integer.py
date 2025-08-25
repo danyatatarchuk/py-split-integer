2e44bee4e04e3a509d2d5ebe49ba35182524b92f
Danylo Tatarchuk <tatarchuk.danil06@gmail.com>
2025-08-25 16:06:32 +0300

    fix newline and character

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,26 +1,26 @@
 from app.split_integer import split_integer
 
 
-def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    result = split_integer(10, 3)
-    assert sum(result) == 10
-
-
-def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    result = split_integer(12, 4)
-    assert result == [3, 3, 3, 3]
-
-
-def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    result = split_integer(7, 1)
-    assert result == [7]
-
-
-def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
+def test_length_of_result_should_be_equal_to_number_of_parts() -> None:
     result = split_integer(17, 4)
+    assert len(result) == 4
+
+
+def test_all_elements_should_be_integers() -> None:
+    result = split_integer(17, 4)
+    assert all(isinstance(x, int) for x in result)
+
+
+def test_max_minus_min_should_be_less_or_equal_one() -> None:
+    result = split_integer(17, 4)
+    assert max(result) - min(result) <= 1
+
+
+def test_split_integer_example_32_6() -> None:
+    result = split_integer(32, 6)
+    assert result == [5, 5, 5, 5, 6, 6]
+
+
+def test_result_should_be_sorted_by_default() -> None:
+    result = split_integer(32, 6)
     assert result == sorted(result)
-
-
-def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    result = split_integer(2, 5)
-    assert result == [0, 0, 0, 1, 1]
